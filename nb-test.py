1dec25c3c9cc24027bbbc60adae384a8e2b0b48d
Alan <pchiu4@u.rochester.edu>
2021-12-04 16:25:35 -0800

    find the grant information

--- nb-test.py
+++ nb-test.py
@@ -20,15 +20,19 @@ class TestAnalyzer(Analyzer):
         self.timer = 0
         self.prevByte = 0 # setup for no possibility
         self.latencyInfo = []
+        self.currLatencyCount = 0
+        self.DCITimeInfo = []
+
+        self.HFN = 0 # Hyper FN clock
+        self.prevFN = 0
+        self.recentupdateTime = 0
 
     def set_source(self, source):
         Analyzer.set_source(self, source)
 
         source.enable_log("LTE_PHY_PUSCH_Tx_Report")
         source.enable_log("LTE_MAC_UL_Buffer_Status_Internal")
-        # source.enable_log("LTE_MAC_DL_Buffer_Status_Internal")
         source.enable_log("LTE_NB1_ML1_GM_DCI_Info")
-        # source.enable_log("LTE_NB1_ML1_GM_PDSCH_STAT_Ind")
 
         # source.enable_log("LTE_RRC_OTA_Packet")
         # source.enable_log("LTE_NAS_ESM_Plain_OTA_Incoming_Message")
@@ -42,9 +46,6 @@ class TestAnalyzer(Analyzer):
             for packet in msg.data.decode()['Subpackets']:
                 # print (msg.data.decode()['timestamp'])
                 
-                prevSFN = 0
-                prevFN = 0
-                
 
                 for sample in packet['Samples']:
                     self.timer += 1
@@ -54,52 +55,41 @@ class TestAnalyzer(Analyzer):
                     LCIDcount = sample['Number of active LCID']
                     LCID = sample['LCIDs']
                     # Compute the latest LCID object's latency
-                    
-                    # print(sample)
+
+                    # update on hyper FN
+                    if FN < self.prevFN:
+                        self.HFN += 1
+                        self.recentupdateTime = msg.data.decode()['timestamp']
 
                     for i in range(LCIDcount): 
                         Byte = LCID[i]['Total Bytes']
 
                     if self.prevByte < Byte: 
                         self.bufferqueue.append([Byte - self.prevByte, Byte - self.prevByte, self.timer])
-
-                        # print( Byte, prevByte)
                     
                     if self.prevByte >= Byte and self.prevByte > 0: 
                         # data is sending out
                         outdata = self.prevByte - Byte
 
 
+                        # data sent out and one buffer is cleared
                         while len(self.bufferqueue) > 0 and self.bufferqueue[0][0] <= outdata: 
                             Latency = self.timer - self.bufferqueue[0][2]
-                            # Latency = time.perf_counter() - self.bufferqueue[0][1]
-                            # print(self.timer, self.bufferqueue[0])
-                            print("Latency is: ", Latency, "with size of ", self.bufferqueue[0][0])
-                            self.latencyInfo.append(Latency)
+                            
+                            # correct timestamp for FN and SFN info here
+                            self.latencyInfo.append([Latency, self.bufferqueue[0][0], FN, SFN, self.HFN])
+                            
+                            # store latency, data size, finished time
                             outdata -= self.bufferqueue[0][0]
                             self.bufferqueue.popleft()
                             
-
+                        # when data are sent out but buffer not yet empty
                         if outdata > 0: 
                             self.bufferqueue[0][0] -= outdata
 
-                        # if self.bufferqueue[0][0] > outdata: 
-                        #     Latency = self.timer - self.bufferqueue[0][1]
-                        #     # print("Latency info: ", Latency)
-                        #     self.bufferqueue[0][0] -= outdata
-
                     
                     self.prevByte = Byte
-                        
-
-                    # if prevSFN != SFN: 
-                    #     prevSFN = SFN
-                    # if prevFN != FN: 
-                    #     prevFN = FN
-                    # print("SFN: ", SFN)
-                    # print("FN: ", FN)
-                    # print("LCID: ", LCID)
-                    # print("LCount", LCIDcount)
+                    self.prevFN = FN
 
                     # subframe number (SFN)  +1 -> %10 every ms
                     # frame number (FN) + 1 every 10 ms
@@ -114,30 +104,87 @@ class TestAnalyzer(Analyzer):
                
         if msg.type_id == "LTE_NB1_ML1_GM_DCI_Info":
             # print(msg.data.decode())
+
             for record in msg.data.decode()['Records']:
                 # print(record)
                 UL_grant = False
                 DL_grant = False
                 
                 if record['UL Grant Present'] == 'True': 
-                    UL_grant = True # grant and the time data is sent out
+                    UL_grant = True # grant and when data is sent out
                 elif record['DL Grant Present'] == 'True':
                     DL_grant = True
-                
+
 
                 if UL_grant: 
                     FN = record['NPDCCH Timing SFN']
                     SFN = record['NPDCCH Timing Sub FN']
-                    self.latencyInfo
-                    print(FN, SFN)
+                    HFN = 0
+                    # if FN < self.DCIprevFN: 
+                    #     self.DCIHFN += 1
+                    
+                    # judge if recently add HFN
+                    # 100 ms
 
-                # print(record)
+                    diff = msg.data.decode()['timestamp'] - self.recentupdateTime
+                    if diff.total_seconds() < 0.1: # recent update of HFN trigger
+                        if FN > 950: # try 
+                            HFN = self.HFN + 1
+                        else: 
+                            HFN = self.HFN
+                    else: 
+                        if FN < 10:
+                            HFN = self.HFN - 1
+                        else: 
+                            HFN = self.HFN
+
+                    self.DCITimeInfo.append([FN,SFN, HFN])
+                    
                 if DL_grant: 
                     FN = record['NPDCCH Timing SFN']
                     SFN = record['NPDCCH Timing Sub FN']
                     # print(FN, SFN)
 
 
+def computeULgrant(LatencyInfo, ULgrantInfo): 
+    
+    prevendTime = 0
+    prevendFN = 0
+    HFN = 0
+
+    # find the UL grant information for each computed latency
+    for i in range(len(LatencyInfo)):
+
+        Latency = LatencyInfo[i][0]
+        size = LatencyInfo[i][1]
+        endFN = LatencyInfo[i][2]
+        endSFN = LatencyInfo[i][3]
+        # HFN = LatencyInfo[i][4]
+        endTime = endFN*10 + endSFN # + HFN * 10240
+
+        startTime = endTime - Latency
+        # startTemp = startTime - HFN * 10240
+        # startFN = startTemp // 10
+        # startSFN = startTemp % 10
+
+        lastfound = 0
+        grant_gap = 9 # proper grant gap information
+
+        # closest one UL grant before endTime
+        for j in range(lastfound, len(ULgrantInfo)): 
+            newTime = ULgrantInfo[j][0]*10 + ULgrantInfo[j][1] 
+            if newTime + grant_gap == endTime: # look for closest grant to end time
+                # j -= 1
+                lastfound = j
+                break
+
+        grantFN = ULgrantInfo[lastfound][0]
+        grantSFN = ULgrantInfo[lastfound][1]
+        grantTimestamp = grantFN*10 + grantSFN
+        waiting = grantTimestamp - startTime
+        grant = endTime - grantTimestamp # 9 
+        print("UL waiting: ", waiting, "UL grant: ", grant, "Latency: ", Latency)
+        
 
 # To use python nb-test.py /path/to/.mi2log
 src = OfflineReplayer()
@@ -149,3 +196,12 @@ analyzer = TestAnalyzer()
 analyzer.set_source(src)
 
 src.run()
+
+# process offline
+LatencyInfo = analyzer.latencyInfo
+ULgrantInfo = analyzer.DCITimeInfo
+computeULgrant(LatencyInfo, ULgrantInfo)
+
+
+
+    
